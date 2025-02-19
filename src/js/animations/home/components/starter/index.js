3560b286f6c7052c9e4f75593c60c13e124c906e
mahana snow <snowmahana.fitt@gmail.com>
2025-02-19 13:20:29 +0100

    Build du projet

--- src/js/animations/home/components/starter/index.js
+++ src/js/animations/home/components/starter/index.js
@@ -189,6 +189,50 @@ export function initHomeStarter() {
 
 
   document.addEventListener('DOMContentLoaded', initTextureReveal);
+// document.querySelectorAll(".row-2").forEach(row => {
+//   const previewWrapper = row.querySelector(".preview-wrapper");
 
+//   gsap.set(previewWrapper, { scale: 0, display: "none" });
+
+//   ScrollTrigger.create({
+//     trigger: row,
+//     start: "top 50%",
+//     end: "bottom 50%",
+//     onEnter: () => {
+//       gsap.set(previewWrapper, { display: "block" });
+//       gsap.fromTo(previewWrapper,
+//         { scale: 0 },
+//         { scale: 1, duration: 0.15, ease: "power1.out" }
+//       );
+//       row.classList.add('active');
+//     },
+//     onLeave: () => {
+//       gsap.to(previewWrapper, {
+//         scale: 0,
+//         duration: 0.15,
+//         ease: "power1.in",
+//         onComplete: () => gsap.set(previewWrapper, { display: "none" })
+//       });
+//       row.classList.remove('active');
+//     },
+//     onEnterBack: () => {
+//       gsap.set(previewWrapper, { display: "block" });
+//       gsap.fromTo(previewWrapper,
+//         { scale: 0 },
+//         { scale: 1, duration: 0.15, ease: "power1.out" }
+//       );
+//       row.classList.add('active');
+//     },
+//     onLeaveBack: () => {
+//       gsap.to(previewWrapper, {
+//         scale: 0,
+//         duration: 0.15,
+//         ease: "power1.in",
+//         onComplete: () => gsap.set(previewWrapper, { display: "none" })
+//       });
+//       row.classList.remove('active');
+//     }
+//   });
+// });
 
 }
--- src/js/pages/home/index.js
+++ src/js/pages/home/index.js
@@ -1,5 +1,5 @@
 // import animations file entry path
-import { initHomeAnimation } from "../../animations/home";
+import { initHomeAnimation } from "../../animations/home/index.js";
 
 //import components file entry path
 
